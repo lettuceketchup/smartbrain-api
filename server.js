26f761110ba301fa002509c0a3510766887a11db
lettuceketchup <lettuceketchup@users.noreply.github.com>
2020-05-02 23:27:05 +0530

    Bugs

--- server.js
+++ server.js
@@ -12,8 +12,7 @@ const db = knex({
     client: 'pg',
     connection: {
         connectionString: process.env.DATABASE_URL,
-        ssl: true
-        
+        ssl: true,        
         // host: '127.0.0.1',
         // user: 'postgres',
         // password: 'pradhumn123',
