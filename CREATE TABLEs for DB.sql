6baa3badaa9632d31b2c1327cbc69c2dd536beb6
lettuceketchup <lettuceketchup@users.noreply.github.com>
2020-05-02 22:56:38 +0530

    Adding Heroku database

---
+++ CREATE TABLEs for DB.sql
@@ -0,0 +1,15 @@
+CREATE TABLE login
+(
+    id serial PRIMARY KEY,
+    hash varchar(100) NOT NULL,
+    email text UNIQUE NOT NULL
+);
+
+CREATE TABLE users
+(
+    id serial PRIMARY KEY,
+    name VARCHAR(100),
+    email TEXT UNIQUE NOT NULL,
+    entries bigint DEFAULT 0,
+    joined TIMESTAMP NOT NULL
+);
